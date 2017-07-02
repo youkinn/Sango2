d11ada017b6468a8378834c5a14a33d6c50e188c
叶鑫 <03338277@163.com>
2017-07-03 00:56:14 +0800

    添加个人中心静态页面

---
+++ src/pages/personal/compontents/list-item.compontent.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="list-item" :class="{'list-item--last': bLast}">
+        <div class="list-item__thumb">
+            <i class="icon" :class="sIcon" :style="{color: sIconColor}"></i>
+        </div>
+        <div class="list-item__content">
+            {{sContent}}
+        </div>
+        <div class="list-item__extra">
+            <slot name="extra">{{sExtra}}</slot>
+        </div>
+        <div class="list-item__arrow">
+            <i class="icon icon-arrow-right"></i>
+        </div>
+    </div>
+</template>
+<script>
+'use strict';
+export default {
+    props: {
+        sIcon: {
+            type: String,
+            defalult: ''
+        },
+        sIconColor: {
+            type: String,
+            defalult: ''
+        },
+        sContent: {
+            type: String,
+            defalult: ''
+        },
+        sExtra: {
+            type: String,
+            defalult: ''
+        },
+        bLast: {
+            type: Boolean,
+            defalult: false
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.list-item {
+    display: flex;
+    align-items: center;
+    height: 87px;
+    margin-left: 30px;
+    padding-right: 30px;
+    background-color: #fff;
+    border-bottom: solid #a5a5a5 1px;
+    /*no*/
+    &--last {
+        border-bottom: none;
+    }
+    &__thumb {
+        display: flex;
+        justify-content: flex-start;
+        width: 64px;
+        .icon {
+            font-size: 40px;
+            /*px*/
+        }
+    }
+    &__content {
+        width: 140px;
+        font-size: 32px;
+        /*px*/
+        color: #333;
+        text-align: left;
+    }
+    &__extra {
+        width: 344px;
+        font-size: 28px;
+        /*px*/
+        color: #a5a5a5;
+        text-align: right;
+    }
+    &__arrow {
+        display: flex;
+        justify-content: flex-end;
+        width: 32px;
+        color: #a5a5a5;
+
+        .icon {
+            font-size: 26px;
+            /*px*/
+        }
+    }
+}
+</style>
--- src/pages/personal/personal.page.vue
+++ src/pages/personal/personal.page.vue
@@ -1,14 +1,57 @@
 <template>
-  <div>
+  <section>
     <app-header-component s-title="个人中心"></app-header-component>
-    <h3>personal index</h3>
+  
+    <section class="user-info">
+      <div class="user-info__avatar">
+        <img src="" alt="" width="82" height="82">
+      </div>
+      <div class="user-info__extra">
+        <div class="user-info__nameAndId">
+          <div class="user-info__name">哼哩个哼哼</div>
+          <div class="user-info__id">淘游ID:34325435</div>
+        </div>
+        <div class="user-info__edit">
+          <i class="icon icon-edit"></i>
+        </div>
+      </div>
+    </section>
+  
+    <section class="list">
+      <list-item s-icon="icon-phone" s-icon-color="#00aaee" s-content="修改手机" s-extra="1886***9248"></list-item>
+      <list-item s-icon="icon-task" s-icon-color="#ff9500" s-content="任务">
+        <span slot="extra">还有
+          <span class="list--item__count">1500</span>淘豆未领取</span>
+      </list-item>
+      <list-item s-icon="icon-taodou" s-icon-color="#00c25b" s-content="淘豆" s-extra="10,000,000"></list-item>
+      <list-item s-icon="icon-taobi" s-icon-color="#00aaee" s-content="淘币" s-extra="1,000"></list-item>
+      <list-item s-icon="icon-exchange" s-icon-color="#ff4d73" s-content="礼包兑换"></list-item>
+      <list-item s-icon="icon-coupon" s-icon-color="#ff9500" s-content="卡券包" :b-last="true">
+        <span slot="extra">您有
+          <span class="list--item__count">2</span>个礼包即将到期</span>
+      </list-item>
+    </section>
+  
+    <section class="list">
+      <list-item s-icon="icon-message" s-icon-color="#00c25b" s-content="消息"></list-item>
+      <list-item s-icon="icon-service" s-icon-color="#00aaee" s-content="客服" :b-last="true"></list-item>
+    </section>
+  
+    <section>
+      <div class="logout" @click.prevent="logout">退出登录</div>
+    </section>
+  
     <app-footer-component></app-footer-component>
-    <input type="button" value="退出" @click.prevent="logout">
-  </div>
+  </section>
 </template>
 <script>
 'use strict';
+import ListItem from './compontents/list-item.compontent';
+
 export default {
+  components: {
+    ListItem
+  },
   methods: {
     logout() {
       Vue.ClientHttp(this).POST({}, '/api/logout')
@@ -24,4 +67,76 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+section {
+  margin-bottom: 30px;
+}
+
+.user-info {
+  display: flex;
+  padding: 30px;
+  background-color: #fff;
+  &__avatar {
+    width: 110px;
+
+    img {
+      width: 82px;
+      height: 82px;
+    }
+  }
+  &__extra {
+    display: flex;
+    width: 470px;
+    height: 82px;
+  }
+  &__nameAndId {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 438px;
+    height: 82px;
+  }
+  &__name {
+    height: 44px;
+    font-size: 32px;
+    /*px*/
+    color: #333;
+  }
+  &__id {
+    height: 38px;
+    font-size: 26px;
+    /*px*/
+    color: #a5a5a5;
+  }
+  &__edit {
+    display: flex;
+    justify-content: flex-end;
+    width: 32px;
+    .icon {
+      margin-top: 6px;
+      font-size: 30px;
+      /*px*/
+      color: #999;
+    }
+  }
+}
+
+.list {
+  background-color: #fff;
+  .list--item {
+    &__count {
+      color: #ff4a4a;
+    }
+  }
+}
+
+.logout {
+  height: 88px;
+  line-height: 88px;
+  margin-bottom: 129px;
+  font-size: 32px;
+  /*px*/
+  color: #ff4a4a;
+  text-align: center;
+  background-color: #fff;
+}
 </style>
